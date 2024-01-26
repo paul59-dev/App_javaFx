17a7300f79509ee4642c3a2e1a2df2d518614a6c
Paul <52114403+paul59-dev@users.noreply.github.com>
2024-01-27 00:37:17 +0100

    feat: create new window app

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        primaryStage.show();
+    }
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+}
